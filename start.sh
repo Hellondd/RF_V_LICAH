05e14fe18df6de746234fbf0ad2aebaafd990059
Hellondd <84024619+Hellondd@users.noreply.github.com>
2026-06-24 14:22:31 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Если переменная RUN_LOAD_REGIONS = true, то выполняем скрипт
-if [ "$RUN_LOAD_REGIONS" = "true" ]; then
-    echo "Running load_regions_from_geojson.py..."
-    python scripts/load_regions_from_geojson.py
-fi
+
+echo "Running load_regions_from_geojson.py..."
+python scripts/load_regions_from_geojson.py
+
 
 # Запускаем основное приложение
 alembic upgrade head
