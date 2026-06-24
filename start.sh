c967402e95a0a11022e841b16d91a3c9b887a8f6
Hellondd <84024619+Hellondd@users.noreply.github.com>
2026-06-24 14:32:37 +0300

    Add execution of seed_regions.py in start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@
 echo "Running load_regions_from_geojson.py..."
 python scripts/load_regions_from_geojson.py
 
-
+echo "Running seed_regions.py"
+python scripts/seed_regions.py
 # Запускаем основное приложение
 alembic upgrade head
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
