de880fc866ec15396ef8084d87042350a7f00f90
Hellondd <84024619+Hellondd@users.noreply.github.com>
2026-06-24 14:14:07 +0300

    Reorganize start.sh for migration and loading logic
    Reorder script to apply migrations before starting the server and add conditional loading of regions.

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-set -e
 
-# Применяем миграции
+# Если переменная RUN_LOAD_REGIONS = true, то выполняем скрипт
+if [ "$RUN_LOAD_REGIONS" = "true" ]; then
+    echo "Running load_regions_from_geojson.py..."
+    python scripts/load_regions_from_geojson.py
+fi
+
+# Запускаем основное приложение
 alembic upgrade head
-
-# Запускаем сервер
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
